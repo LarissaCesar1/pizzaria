a6c47d3080cb430ebe6c368e5b52e3fd5b6e00c2
Larissa Cesar <larissavitoriacesar1415@gmail.com>
2022-03-10 14:29:26 -0300

    Fase 8 concluida

--- controllers/PizzasControllers.js
+++ controllers/PizzasControllers.js
@@ -1,7 +1,8 @@
-
 const PizzasControllers = {
     listar: (req, res)=> {
-        res.render('pizzas.ejs');
+        res.render('pizzas.ejs', {PizzasJson});
     }
 };
 module.exports = PizzasControllers;
+
+const PizzasJson = require('../database/Pizzas.json');
