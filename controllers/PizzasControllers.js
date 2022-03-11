e8c2fb4e32c7669c3c500d11f49ff553bc297f45
Larissa Cesar <larissavitoriacesar1415@gmail.com>
2022-03-10 22:57:45 -0300

    Fase 12 concluida

--- controllers/PizzasControllers.js
+++ controllers/PizzasControllers.js
@@ -14,9 +14,19 @@ const PizzasController = {
         res.render('pizza.ejs', {pizza} )
     },
 
+    buscar: (req,res) =>{
+        let busca = req.query.q;
+        if (busca){
+            let result = pizzas.filter(p => p.nome.toUpperCase().includes(busca.toUpperCase()));
+            return res.render('../views/pizzas.ejs', { pizzas: result,busca});
+        }else{
+            return res.redirect('/');
+        }
+ 
+    }
+    
    
-}
-
+};
  
 
 module.exports = PizzasController;
--- routes/PizzasRouter.js
+++ routes/PizzasRouter.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const PizzasControllers= require("../controllers/PizzasControllers");
 
 router.get('/', PizzasControllers.listar);
+router.get('/pizza/busca', PizzasControllers.buscar);
 router.get('/pizzas/:id', PizzasControllers.mostrar);
+
+
 
 module.exports =  router;
